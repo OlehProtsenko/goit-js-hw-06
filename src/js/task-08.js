e247d39a8ea89d26e5a9a534588004d946bf6be6
OlehProtsenko <138534341+OlehProtsenko@users.noreply.github.com>
2023-12-08 13:32:23 +0100

    task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
 
+const registrationForm = document.querySelector(".login-form");
+
+registrationForm.addEventListener("submit", onVerificationForm);
+
+function onVerificationForm(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === "" || password === "") {
+    alert("Усі поля повинні бути заповнені!");
+  } else {
+    const newUser = {
+      email: email,
+      password: password,
+    };
+    console.log(newUser);
+    form.reset();
+  }
+}
