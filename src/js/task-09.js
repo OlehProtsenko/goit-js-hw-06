1fca5084bf0c112ed7f44f5cf58939197773b17f
OlehProtsenko <138534341+OlehProtsenko@users.noreply.github.com>
2023-12-08 13:29:50 +0100

    task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,14 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const body = document.querySelector("body");
+const showColor = document.querySelector(".color");
+const chengeColorBtn = document.querySelector(".change-color");
+
+function onChangeColor() {
+  const bgColor = getRandomHexColor();
+  body.style.backgroundColor = bgColor;
+  showColor.textContent = bgColor;
+}
+
+chengeColorBtn.addEventListener("click", onChangeColor);
